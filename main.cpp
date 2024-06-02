51edbbd3459609a7ea944598930a3b0e9c4656e6
ra-kunnn <aeveralio@gmail.com>
2024-06-02 15:22:28 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <limits>
+
+using namespace std;
+
+int main() {
+
+    cout << "Heylp I am Snole pls give Command. enter HELP for full command list";
+    //should show all possible commands rin para nice
+
+    do{
+
+        cout << "\n\nEnter Command: ";
+        string command;
+        getline(cin, command);
+
+
+
+
+
+    }while();
+    
+}
